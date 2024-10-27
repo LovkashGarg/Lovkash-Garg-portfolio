d8f9c7768132905c6196e24e9377684173ee8499
LovkashGarg <aggarwallovkesh55@gmail.com>
2024-10-27 19:23:20 +0530

    added backend

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -2,5 +2,5 @@ export const contactsData = {
     email: 'lovkashgarg@gmail.com',
     phone: '+919896316033',
     address: 'Pune,Maharastra',
-    sheetAPI: 'http://localhost:5000/api/contact'
+    sheetAPI: 'https://portfolio-back-ysfh.onrender.com/api/contact'
 }
